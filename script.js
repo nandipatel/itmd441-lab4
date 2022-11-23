f0f27b01d98ab12e48d527fe7c6ddfb5c71fce75
nandipatel <60127512+nandipatel@users.noreply.github.com>
2022-11-23 02:49:46 -0600

    js changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+function init(){
+    document.addEventListener('DOMContentLoaded', init);
+    document.getElementById("enter").addEventListener("click", getLoc);
+}
 //grabbing input
-document.getElementById("enter").addEventListener("click", getLoc());
-
-
+document.getElementById("enter").addEventListener("click", getLoc);
 //getting location
 function getLoc(){
+    console.log("working");
     location = document.getElementById("loc");
     partURL = "https://weatherdbi.herokuapp.com/data/weather/";
     urlLink = partURL + location;
-    
+
     const base = urlLink;
     console.log(base);
 }
