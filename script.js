e39bd6a5c13be104fd5482faee866e9bddf1f795
nandipatel <60127512+nandipatel@users.noreply.github.com>
2022-11-29 20:29:49 -0600

    added more js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 function init(){
     document.addEventListener('DOMContentLoaded', init);
-    document.getElementById("enter").addEventListener("click", getLoc);
+    document.getElementById("enter").addEventListener("click", search);
+    document.getElementById("geoloc").addEventListener("click", current);
 }
+
 //grabbing input
-document.getElementById("enter").addEventListener("click", getLoc);
-//getting location
-function getLoc(){
-    console.log("working");
+document.getElementById("enter").addEventListener("click", search);
+// search button & getting location
+function search(){
     location = document.getElementById("loc");
     partURL = "https://weatherdbi.herokuapp.com/data/weather/";
     urlLink = partURL + location;
 
     const base = urlLink;
     console.log(base);
+
+    document.getElementById("weather").value = urlLink;
+}
+
+//grabbing input
+document.getElementById("geoloc").addEventListener("click", current);
+// current geo button
+function current(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
